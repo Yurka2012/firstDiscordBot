cba82cb8c3a78252c8f14d11690e4a53c287dab9
Yurka2012 <tsapiy.yurka@gmail.com>
2023-02-06 14:19:35 +0200

    -
    поправление красоты

--- main.js
+++ main.js
@@ -1,24 +1,23 @@
 const { Client, IntentsBitField, messageLink } = require("discord.js");
 const { token } = require("./config.json");
 const client = new Client({ intents: [32767] });
-const id = '='
+const id = "=";
 
 client.once("ready", () => console.log("Бот онлайн"));
 
-client.on('message', message =>{
-    if(!message.content.startsWith(id) ||message.author.bot) return;
-    const args = message.content.slice(id.length).split(/ +/);
-    const cmd = args.shift().toLowerCase()
+client.on("message", (message) => {
+  if (!message.content.startsWith(id) || message.author.bot) return;
+  const args = message.content.slice(id.length).split(/ +/);
+  const cmd = args.shift().toLowerCase();
 
-    if(cmd === 'привет') {
-        message.channel.send('Ку!')
-    }
-    if(cmd === 'help')  {
-        message.channel.send('Привет, бро! \n Мой преикс: =\n Мои команды: \n привет (бот будет писать Ку!)') 
-    }
-})
+  if (cmd === "привет") {
+    message.channel.send("Ку!");
+  }
+  if (cmd === "help") {
+    message.channel.send(
+      "Привет, бро! \n Мой преикс: =\n Мои команды: \n привет (бот будет писать Ку!)"
+    );
+  }
+});
 
 client.login(token);
-
-
-
