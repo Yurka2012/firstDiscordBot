bc4a3ec8806317ae748bd6727abefd234440a668
Yurka2012 <tsapiy.yurka@gmail.com>
2023-02-06 13:31:34 +0200

    ш0оропм

--- main.js
+++ main.js
@@ -1,7 +1,15 @@
-const { Client, IntentsBitField } = require("discord.js");
+const { Client, IntentsBitField, messageLink } = require("discord.js");
 const { token } = require("./config.json");
-const client = new Client({ intents: [IntentsBitField.Flags.Guilds] });
+const client = new Client({ intents: [32767] });
+const id = 'v='
 
 client.once("ready", () => console.log("Бот онлайн"));
 
+client.on('message', message =>{
+    if(!message.content)
+})
+
 client.login(token);
+
+
+
