5626b04c4aa1369b14501395fbdbbd0fb670128e
Yurka2012 <tsapiy.yurka@gmail.com>
2023-02-06 19:45:37 +0200

    вцый

--- main.js
+++ main.js
@@ -9,13 +9,19 @@ const fs = require('fs');
 
 client.commands = new Collection();
 
-const cmdFiles = fs.readdirSync('./cmd'.filter(file => file.endWith('.js')));
+const commandsPath = path.join(__dirname, 'cmd');
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
 
-for(const file of commandFiles) {
-  const cmd = require(`./cmd/${file}`);
-  client.cmd.set(cmd.name , cmd);
+for (const file of commandFiles) {
+    const filePath = path.join(commandsPath, file);
+    const command = require(filePath);
+    // Set a new item in the Collection with the key as the command name and the value as the exported module
+    if ('data' in command && 'execute' in command) {
+        client.commands.set(command.data.name, command);
+    } else {
+        console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+    }
 }
-
 client.on("message", (message) => {
   if (!message.content.startsWith(id) || message.author.bot) return;
   const args = message.content.slice(id.length).split(/ +/);
