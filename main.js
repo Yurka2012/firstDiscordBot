ac483841167dd3c3f1c360836f8dd23e6c42f106
Yurka2012 <tsapiy.yurka@gmail.com>
2023-02-06 11:22:22 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const { Client, IntentsBitField } = require("discord.js");
 const { token } = require("./config.json");
-const client = new Client({ intents: [IntentsBitField.FLAGS.GUILDS] });
+const client = new Client({ intents: [IntentsBitField.Flags.Guilds] });
 
 client.once("ready", () => console.log("Бот онлайн"));
 
