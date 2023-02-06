93bfd9b17ac59312b7b695c625bca43662e8ab1f
Yurka2012 <tsapiy.yurka@gmail.com>
2023-02-06 19:48:30 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,12 @@
-const { Client, IntentsBitField, messageLink, Discord, Collection } = require("discord.js");
+const fs = require("node:fs");
+const path = require("node:path");
+const {
+  Client,
+  IntentsBitField,
+  messageLink,
+  Discord,
+  Collection,
+} = require("discord.js");
 const { token } = require("./config.json");
 const client = new Client({ intents: [32767] });
 const id = "=";
@@ -9,18 +17,22 @@ const fs = require('fs');
 
 client.commands = new Collection();
 
-const commandsPath = path.join(__dirname, 'cmd');
-const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+const commandsPath = path.join(__dirname, "cmd");
+const commandFiles = fs
+  .readdirSync(commandsPath)
+  .filter((file) => file.endsWith(".js"));
 
 for (const file of commandFiles) {
-    const filePath = path.join(commandsPath, file);
-    const command = require(filePath);
-    // Set a new item in the Collection with the key as the command name and the value as the exported module
-    if ('data' in command && 'execute' in command) {
-        client.commands.set(command.data.name, command);
-    } else {
-        console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
-    }
+  const filePath = path.join(commandsPath, file);
+  const command = require(filePath);
+  // Set a new item in the Collection with the key as the command name and the value as the exported module
+  if ("data" in command && "execute" in command) {
+    client.commands.set(command.data.name, command);
+  } else {
+    console.log(
+      `[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`
+    );
+  }
 }
 client.on("message", (message) => {
   if (!message.content.startsWith(id) || message.author.bot) return;
@@ -35,11 +47,9 @@ client.on("message", (message) => {
       "Привет, бро! \n Мой преикс: =\n Мои команды: \n привет (бот будет писать Ку!) \n пинг (бот отвечает понг!)"
     );
   }
-  if(cmd === 'пинг') {
-    message.channel.send(
-      'Понг'
-    )
+  if (cmd === "пинг") {
+    message.channel.send("Понг");
   }
 });
-                                                                                    
+
 client.login(token);
