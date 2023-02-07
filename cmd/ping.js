3db940e6192c3ab5969bac9e527a9ab1dfda3c70
Yurka2012 <tsapiy.yurka@gmail.com>
2023-02-07 07:54:33 +0200

    Update ping.js

--- cmd/ping.js
+++ cmd/ping.js
@@ -1,4 +1,7 @@
 module.exports = {
     name: 'пинг',
-    desription: 'Эта команда пишет в ответ на запрос Понг!'
+    desription: 'Эта команда пишет в ответ на запрос Понг!',
+    execute(message, args) {
+        message.channel.send('Понг!');
+    }
 }
